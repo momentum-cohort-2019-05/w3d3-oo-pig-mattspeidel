e9561581579dac9b6d413f944d2eb8589c2747ae
mattspeidel <mattspeidel@gmail.com>
2019-06-12 12:58:05 -0400

    initial commit

---
+++ pig.py
@@ -0,0 +1,27 @@
+import random
+
+class Die:
+    def __init__(self):
+
+    def roll(self):
+
+class Turn:
+    def __init__(self):
+
+    def hold(self):
+
+    def play(self):
+
+    def bust(self):
+
+class Score:
+    def __init__(self):
+
+    def add(self):
+
+    def win(self):
+
+class Players:
+    def __init__(self):
+    
+    def playercount(self):
